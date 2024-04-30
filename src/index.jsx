ee4a8bc1d45758f16868823787c866a1213b5108
tasmimdristy7 <tasmimdristy7@gmail.com>
2024-05-01 00:59:52 +0600

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap" />
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename="/portfolio-site"> {/* Add basename prop here */}
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
